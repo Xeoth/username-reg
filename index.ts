d37ce217f500fa7417ecbfc4a42d249b1aef2aa2
Xeoth <46075322+Xeoth@users.noreply.github.com>
2020-10-24 20:21:04 +0200

    switched auth method

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
+import basicAuth from "express-basic-auth";
 import dotenv from "dotenv";
 dotenv.config();
 
-import authenticate from "./middleware/auth.js";
 import usernameRouter from "./routes/usernames.js";
 
 mongoose.connect("mongodb://localhost/username_logger", {
@@ -11,10 +11,20 @@ mongoose.connect("mongodb://localhost/username_logger", {
   useNewUrlParser: true,
 });
 
+const password = process.env.LOGGER_PASSWORD;
+
 const app = express();
 
 app.use(express.json());
 
-app.use("/username", authenticate, usernameRouter);
+app.use(
+  basicAuth({
+    users: {
+      admin: password,
+    },
+  })
+);
+
+app.use("/username", usernameRouter);
 
 app.listen(process.env.PORT, () => console.log("9k list"));
