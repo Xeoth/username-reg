ffa9aee9ef89297d506a2c94060ac33951ddea12
Xeoth <46075322+Xeoth@users.noreply.github.com>
2020-10-24 16:57:35 +0200

    added the POST method in usernames

--- models/username.ts
+++ models/username.ts
@@ -1,4 +1,3 @@
-import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from "constants";
 import mongoose from "mongoose";
 
 const usernameSchema = new mongoose.Schema({
--- routes/usernames.ts
+++ routes/usernames.ts
@@ -1,11 +1,34 @@
 import router from "express";
 import mongoose from "mongoose";
 
+import usernameModel from "../models/username.js";
+
 const usernameRouter = router();
 const db = mongoose.connection;
 
-usernameRouter.get("/", (req, res) => {
-  const { userID } = req.params;
+// usernameRouter.get("/", (req, res) => {
+//   const { userID } = req.params;
+
+//   usernameModel
+// });
+
+usernameRouter.post("/", (req, res) => {
+  const { userID, username } = req.body;
+
+  const usernameDoc = new usernameModel({
+    id: userID,
+    name: username,
+    date: new Date(),
+  });
+
+  usernameDoc.save((err, saved) => {
+    if (err) {
+      console.error(err);
+      return res.sendStatus(500);
+    }
+
+    res.status(201).send(saved);
+  });
 });
 
 export default usernameRouter;
